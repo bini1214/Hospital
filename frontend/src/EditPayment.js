b9fa864af7b3187e660e1bed1617e9445dc85136
biniyam <biniyamabera14@gmail.com>
2024-01-19 19:02:41 +0300

    update

--- frontend/src/EditPayment.js
+++ frontend/src/EditPayment.js
@@ -1,26 +1,24 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from "react-router-dom";
 //import EditPatient from './EditPatient';
 
 function EditPayment() {
-const [data,setData]=useState({
-    bi_id:'',
-    p_id:'', 
-    reason:'',
-    amount:'',
-   
-})
+  const [data, setData] = useState({
+    bi_id: "",
+    p_id: "",
+    reason: "",
+    amount: "",
+  });
 
+  const navigate = useNavigate();
+  const { id } = useParams();
 
-const navigate=useNavigate();  
-const {id}=useParams();
-
-
-useEffect(() => {
-    axios.get(`http://localhost:8081/get/payment/${bi_id}/${p_id}`)
-      .then(res => {
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8081/get/payment/${bi_id}/${p_id}`)
+      .then((res) => {
         setData({
           ...data,
           bi_id: res.data.Result[0].bi_id,
@@ -29,94 +27,102 @@ useEffect(() => {
           amount: res.data.Result[0].amount,
         });
       })
-      .catch(err => {
+      .catch((err) => {
         console.log("An error has occurred:");
         console.log(err);
       });
   }, [bi_id, p_id]);
-  
+
   const handleSubmit = async (event) => {
     event.preventDefault();
-  
+
     // Convert the amount to a decimal
     const decimalAmount = parseFloat(data.amount);
-  
+
     // Update the data state with the decimal amount
     setData({ ...data, amount: decimalAmount });
-  
-    axios.put(`http://localhost:8081/update/payment/${data.bi_id}/${data.p_id}`, data)
-      .then(res => {
+
+    axios
+      .put(
+        `http://localhost:8081/update/payment/${data.bi_id}/${data.p_id}`,
+        data
+      )
+      .then((res) => {
         if (res.data.Status === "Success") {
-          navigate('payment');
+          navigate("payment");
         }
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   };
-  
 
-
-return (
-    <div className='d-flex flex-column align-items-center pt-4'>
+  return (
+    <div className="d-flex flex-column align-items-center pt-4">
       <h2>Update Payment</h2>
-      <form className='row g-3 w-50' onSubmit={handleSubmit}>
-        <div className='col-12'>
-          <label htmlFor="bi_id" className='form-label'>Payment_ID</label>
+      <form className="row g-3 w-50" onSubmit={handleSubmit}>
+        <div className="col-12">
+          <label htmlFor="bi_id" className="form-label">
+            Payment_ID
+          </label>
           <input
-            type='text'
-            className='form-control'
-            id='bi_id'
+            type="text"
+            className="form-control"
+            id="bi_id"
             placeholder={data.bi_id}
-            onChange={e => setData({ ...data, bi_id: e.target.value })}
+            onChange={(e) => setData({ ...data, bi_id: e.target.value })}
             value={data.bi_id}
           />
         </div>
 
-        <div className='col-12'>
-          <label htmlFor="p_id" className='form-label'>Patient_ID</label>
+        <div className="col-12">
+          <label htmlFor="p_id" className="form-label">
+            Patient_ID
+          </label>
           <input
-            type='text'
-            className='form-control'
-            id='p_id'
+            type="text"
+            className="form-control"
+            id="p_id"
             placeholder={data.p_id}
-            onChange={e => setData({ ...data, p_id: e.target.value })}
+            onChange={(e) => setData({ ...data, p_id: e.target.value })}
             value={data.p_id}
           />
         </div>
 
-
-        <div className='col-12'>
-          <label htmlFor="inputFirstName" className='form-label'>Reason</label>
+        <div className="col-12">
+          <label htmlFor="inputFirstName" className="form-label">
+            Reason
+          </label>
           <input
-            type='text'
-            className='form-control'
-            id='inputFirstN'
-            placeholder='inter payment reason'
-            onChange={e => setData({ ...data, reason: e.target.value })}
+            type="text"
+            className="form-control"
+            id="inputFirstN"
+            placeholder="inter payment reason"
+            onChange={(e) => setData({ ...data, reason: e.target.value })}
             value={data.reason}
           />
         </div>
 
-
-       
-        <div className='col-12'>
-          <label htmlFor="inputSalary" className='form-label'>Amount</label>
+        <div className="col-12">
+          <label htmlFor="inputSalary" className="form-label">
+            Amount
+          </label>
           <input
-            type='number'
-            step="0.01" 
-            className='form-control'
-            id='inputSalary'
-            placeholder='Enter amount in Birr'
-            onChange={e => setData({ ...data, amount: e.target.value })}
+            type="number"
+            step="0.01"
+            className="form-control"
+            id="inputSalary"
+            placeholder="Enter amount in Birr"
+            onChange={(e) => setData({ ...data, amount: e.target.value })}
             value={data.amount}
           />
         </div>
-        <div className='col-12 mb-3'>
-          <button type='submit' className='btn btn-primary'>Update</button>
+        <div className="col-12 mb-3">
+          <button type="submit" className="btn btn-primary">
+            Update
+          </button>
         </div>
       </form>
     </div>
   );
-  
 }
 
-export default EditPayment
+export default EditPayment;
--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,72 +1,77 @@
-import React, { useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
- import './style.css'
- import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-function Login() { 
-    
-    const [values,setValues]=useState({
-        email:'',
-        password:''
-    })
-    const navigate=useNavigate()
+function Login() {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
 
-    axios.defaults.withCredentials=true;
+  axios.defaults.withCredentials = true;
 
-const [error,setError]=useState('');
+  const [error, setError] = useState("");
 
-
-const handleSubmit=(event) =>{
+  const handleSubmit = (event) => {
     event.preventDefault();
-    axios.post('http://localhost:8081/login',values)
-    .then(res=>
-        {
-        if(res.data.Status==='Success')
-        {
-           navigate('/dash');
-        }    
-        else{
-            setError(res.data.Error);
+    axios
+      .post("http://localhost:8081/login", values)
+      .then((res) => {
+        if (res.data.Status === "Success") {
+          navigate("/dash");
+        } else {
+          setError(res.data.Error);
         }
-        })
-    .catch(err=>console.log(err));
-} 
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
-    <div className='d-flex justify-content-center align-items-center vh-100 loginPage'>
-        <div className='p-3 w-35 border loginForm'>
+    <div className="d-flex justify-content-center align-items-center vh-100 loginPage">
+      <div className="p-3 w-35 border loginForm">
+        <div className="text-danger">{error && error}</div>
 
-           <div className='text-danger'>
-            {error &&error}
-           </div>
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="email">
+              <strong>Email</strong>
+            </label>
+            <input
+              type="email"
+              placeholder="Enter Email"
+              name="email"
+              onChange={(e) => setValues({ ...values, email: e.target.value })}
+              className="form-control rounded-0"
+            />
+          </div>
 
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
-                <div className='mb-3'>
-                    <label htmlFor='email'><strong>Email</strong></label>
-                    <input type="email" placeholder='Enter Email' name='email'
-                     onChange={e=>setValues({...values,email: e.target.value})} className='form-control rounded-0'
-                    />
-                </div>
+          <div className="mb-3">
+            <label htmlFor="password">
+              <strong>Password</strong>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              name="password"
+              onChange={(e) =>
+                setValues({ ...values, password: e.target.value })
+              }
+              className="form-control rounded-0"
+            />
+          </div>
 
-                <div className='mb-3'>
-                    <label htmlFor='password'><strong>Password</strong></label>
-                    <input type='password' placeholder='Enter Password' name='password'
-                       onChange={e=>setValues({...values,password: e.target.value})} className='form-control rounded-0'/>
-                </div>
-
-                <button type='submit' className='btn btn-success w-100 rounded-0' >Log in</button>
-                <p>you must be an authorized person</p>
-               
-
-
-            </form>
-
-        </div>
-      
+          <button type="submit" className="btn btn-success w-100 rounded-0">
+            Log in
+          </button>
+          <p>you must be an authorized person</p>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
